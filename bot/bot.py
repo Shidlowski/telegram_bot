f6e540b236bbe64a7f1837b7cf5ab22a8d7c3f85
Alexander Shidlowski <44673278+Shidlowski@users.noreply.github.com>
2019-10-16 06:55:52 +0300

    sen_echo fixed

--- bot/bot.py
+++ bot/bot.py
@@ -12,12 +12,23 @@ mes ="""Команды бота Ushibibot:
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-        bot.send_message(message.chat.id, mes)
+    bot.send_message(message.chat.id, mes)
+    bot.set_chat_photo(message.chat.id,"logo.jpg")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-	#bot.reply_to(message, "Howdy, how are you doing?")
-    if((message.text).lower()=="команды"):
+    mess=(message.text).lower()
+    if(mess=="команды"):
         bot.send_message(message.chat.id, mes)
+    elif(mess=="погода на сегодня"):
+        bot.send_message(message.chat.id, mes)
+    elif(mess=="погода на завтра"):
+        bot.send_message(message.chat.id, mes)
+    elif(mess=="смешной анекдот"):
+        bot.send_message(message.chat.id, mes)
+    elif(mess=="мемас"):
+        bot.send_message(message.chat.id, mes)
+    else:
+        bot.reply_to(message, "Простите, но еще нет такой команды!")
 
 bot.polling(none_stop=True)
